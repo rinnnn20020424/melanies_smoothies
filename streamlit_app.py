579f979a19f04e45ab913f667cc6e99e6abb2767
rinnnn20020424 <rkanbara.hbk@gmail.com>
2025-12-05 16:20:06 +0900

    Update streamlit_app.py
    Breakfast Menuのタイトルに続くメニュー項目を追記しました。

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title('My Parents New Healthy Diner')
-st.write(
-  ""Breakfast Menu"")
-  st.write("""Omega 3 & Blueberry Oatmeal
- Kale, Spinach & Rocket Smoothie
- Hard-Boiled Free-Range Egg""")
+st.header(
+  "Breakfast Menu")
+  st.write('Omega 3 & Blueberry Oatmeal')
+st.write('Kale, Spinach & Rocket Smoothie')
+ st.write('Hard-Boiled Free-Range Egg')
  
 name_on_order = st.text_input("Name on Smoothie:")
 st.write('The name on your Smoothie will be:',name_on_order)
