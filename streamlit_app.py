f57dcfb597c565ffaf31085a06c6a404e765e094
rinnnn20020424 <rkanbara.hbk@gmail.com>
2025-12-07 22:19:32 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
     
     if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+      session.sql(my_insert_stmt).collect()
       st.success('Your Smoothie is orderd,'+name_on_order+'!', icon="✅")
 
 #New section to display smoothiefroot nutrition information
